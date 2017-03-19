5ac3048965fbf33bce0eb9ca8dd81fb7b619f696
anderson <andersonrochatavares@gmail.com>
2017-03-18 21:56:10 -0300

    preparing RLExperiment to save/load Q tables

--- src/rl/RLExperiment.java
+++ src/rl/RLExperiment.java
@@ -1,11 +1,20 @@
 package rl;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import ai.metabot.learning.model.MicroRTSState;
 import burlap.behavior.stochasticgames.GameEpisode;
+import burlap.behavior.stochasticgames.agents.interfacing.singleagent.LearningAgentToSGAgentInterface;
+import burlap.behavior.valuefunction.QValue;
 import burlap.debugtools.DPrint;
 import burlap.mdp.stochasticgames.agent.SGAgent;
 import burlap.mdp.stochasticgames.world.World;
@@ -18,34 +27,68 @@ import burlap.mdp.stochasticgames.world.World;
  *
  */
 public class RLExperiment {
-
-	List<SGAgent> players;
-	World microRTSAbstraction;
-	public RLExperiment(List<SGAgent> players, World microRTSAbstraction) {
+	
+	public static void main(String[] args){
+		//loads parameters from file
+		Map<String, Object> parameters = null;
 		
-		if (players.size() < 2){
+		try {
+			parameters = RLParameters.getInstance().loadFromFile(args[1]);
+		} catch (SAXException | IOException | ParserConfigurationException e) {
+			System.err.println("An error has occurred...");
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		//adds players to the world
+		World gameWorld = (World) parameters.get(RLParamNames.ABSTRACTION_MODEL);
+		List<SGAgent> agents = (List<SGAgent>) parameters.get(RLParamNames.PLAYERS);
+		
+		if(agents.size() < 2){
 			throw new RuntimeException("Less than 2 players were specified for the experiment");
 		}
 		
-		//makes the players join the 'game' 
-		this.players = players;
-		this.microRTSAbstraction = microRTSAbstraction;
-		for(SGAgent player : players) {
-			microRTSAbstraction.join(player);
+		for(SGAgent agent : agents){
+			gameWorld.join(agent);
 		}
-	}
-	
-	public void run(){
-		//DPrint.toggleCode(microRTSAbstraction.getDebugId(), false);
-
+		
+		
+		//performs training
 		System.out.println("Starting training");
-		int ngames = 100;
-		List<GameEpisode> episodes = new ArrayList<GameEpisode>(ngames);
+		
+		// don't have the world print out debug info (uncomment if you want to see it!)
+		//DPrint.toggleCode(theWorld.getDebugId(), false);
+		int numEpisodes = (int) parameters.get(RLParamNames.EPISODES);
+		List<GameEpisode> episodes = new ArrayList<GameEpisode>(numEpisodes);
 		PrintWriter output = null;
-		File file = new File("output.txt");
-		if (file.exists()) {
-			file.delete();
+		
+		//retrieves output dir
+		String outDir = (String) parameters.get(RLParamNames.OUTPUT_DIR);
+		
+		for (int episodeNumber = 0; episodeNumber < numEpisodes; episodeNumber++) {
+			GameEpisode episode = gameWorld.runGame();
+			episodes.add(episode);
+			
+			System.out.print(String.format("\bGame: #%7d finished.", episodeNumber));
+			
+			/*if (i % 10 == 0) {
+				System.out.println("Game: " + i + ": " + episode.maxTimeStep());
+			}*/
+			//writes episode data and q-values
+			episode.write(String.format("%s/episode_%d", outDir, episodeNumber));
+			for(SGAgent agent : gameWorld.getRegisteredAgents()){
+				/**
+				 * TODO here's what you gonna do: instead of referring to
+				 * SGAgent, you'll store the primitive agent: QLearning, MAQL, wathever.
+				 * Then here you'll write out the q table of that primitive agent. 
+				 * It would be something like:
+				 * primitiveAgent.writeQTable(String.format("%s/q_%s_%d", outDir, agentName, i))
+				 * 
+				 * Deal?
+				 */
+				
+			}
 		}
 	}
-
+		
 }
