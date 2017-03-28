fdf7ce2104e232d3f9d8967b6a7d5db92712be58
anderson <andersonrochatavares@gmail.com>
2017-03-28 19:06:59 -0300

    GameStage made public for testability

---
+++ src/ai/metabot/learning/model/GameStage.java
@@ -0,0 +1,10 @@
+package ai.metabot.learning.model;
+
+public enum GameStage{
+	OPENING, 	//0 - 599 frames
+	EARLY,		//600 - 1199
+	MID,		//1200 - 1799
+	LATE,		//1800 - 2399
+	END,		//2400 - 3000
+	FINISHED	//whenever the game is over
+}
--- src/ai/metabot/learning/model/MicroRTSState.java
+++ src/ai/metabot/learning/model/MicroRTSState.java
@@ -6,15 +6,6 @@ import java.util.List;
 import burlap.mdp.core.state.State;
 import rts.GameState;
 
-enum GameStage{
-	OPENING, 	//0 - 599 frames
-	EARLY,		//600 - 1199
-	MID,		//1200 - 1799
-	LATE,		//1800 - 2399
-	END,		//2400 - 3000
-	FINISHED	//whenever the game is over
-}
-
 public class MicroRTSState implements State{
 	
 	GameStage stage;
