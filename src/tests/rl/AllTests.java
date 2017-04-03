0d0cb6fe452f581364d2ba21e05ef50d2b64ca2e
anderson <andersonrochatavares@gmail.com>
2017-04-03 16:34:40 -0300

    new reward functions properly tested

--- src/tests/rl/AllTests.java
+++ src/tests/rl/AllTests.java
@@ -6,6 +6,8 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import tests.rl.adapters.learners.PersistentMultiAgentQLearningTest;
 import tests.rl.adapters.learners.SGQLearningAdapterTest;
+import tests.rl.models.common.TestSimpleWeightedFeatures;
+import tests.rl.models.common.TestSimpleWeightedFeaturesTerminal;
 import tests.rl.models.simplecounting.AggregateJAMTest;
 
 @RunWith(Suite.class)
@@ -13,7 +15,9 @@ import tests.rl.models.simplecounting.AggregateJAMTest;
 	RLParametersTest.class, 
 	SGQLearningAdapterTest.class,
 	PersistentMultiAgentQLearningTest.class,
-	AggregateJAMTest.class
+	AggregateJAMTest.class,
+	TestSimpleWeightedFeatures.class,
+	TestSimpleWeightedFeaturesTerminal.class,
 })
 
 public class AllTests {
