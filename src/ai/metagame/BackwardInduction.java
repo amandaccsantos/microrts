3f5d2587c8f3a688af0ca96d9fb275a524db3544
anderson <andersonrochatavares@gmail.com>
2017-04-18 16:02:26 -0300

    save/load knowledge implemented. test?

--- src/ai/metagame/BackwardInduction.java
+++ src/ai/metagame/BackwardInduction.java
@@ -2,9 +2,11 @@ package ai.metagame;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -12,7 +14,16 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.jdom.JDOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import burlap.mdp.core.TerminalFunction;
 import burlap.mdp.core.action.Action;
@@ -207,7 +218,6 @@ public class BackwardInduction implements PersistentLearner {
 			opponentPolicy[i] = Double.parseDouble(parts[i + agentPolicy.length + 1]);
 		}
 		
-		//TODO now we have the policy, how about the value?
 		double stateValue = 0;
 		int agentActionIndex = 0;
 		for(ActionType a : type.actions){
@@ -267,13 +277,159 @@ public class BackwardInduction implements PersistentLearner {
 
 	@Override
 	public void saveKnowledge(String path) {
-		// TODO Auto-generated method stub
+		if (!(domain instanceof EnumerableSGDomain)){
+			System.err.println("Can only save knowledge for EnumerableSGDomains, this one is: " + domain.getClass().getName());
+			return;
+		}
+		
+		BufferedWriter fileWriter;
+		try {
+			fileWriter = new BufferedWriter(new FileWriter(path));
+
+			EnumerableSGDomain enumDomain = (EnumerableSGDomain) domain;
+			
+			// xml root node
+			fileWriter.write("<knowledge>\n\n"); 
+			
+			// information about who is saving knowledge, i.e., myself
+			// this might be useful when retrieving the joint action later
+			fileWriter.write(String.format(
+				"<learner name='%s' type='%s' id='%d' />\n\n", 
+				agentName(), this.getClass().getName(), agentNumber
+			));
+			
+			// a friendly remark
+			fileWriter.write(
+				"<!-- Note: 'ja' stands for joint action\n"
+				+ "Joint action name is agent0Action;agent1Action;... "
+				+ " always in this order -->\n\n"
+			);
+			
+			for (State s : enumDomain.enumerate()) {
+				// opens state tag
+				fileWriter.write(
+					String.format(Locale.ROOT, "<state id='%s' value='%f'>\n", s, V.get(s))
+				); 
+				
+				// runs through joint actions and write their values
+				/*List<JointAction> jointActions = JointAction.getAllJointActions(
+					s, 
+					world.getRegisteredAgents()
+				);*/
+				
+				for(ActionType playerAction : this.type.actions){
+					for(ActionType opponentAction : this.type.actions){
+						JointAction ja = new JointAction();
+						ja.addAction(playerAction.associatedAction(null));
+						ja.addAction(opponentAction.associatedAction(null));
+						
+						// writes the joint action tag
+						// action name is agent0Action;agent1Action;...
+						// always in this order
+						fileWriter.write(String.format(
+							Locale.ROOT,
+							"\t<ja name='%s' value='%f' />\n",
+							ja.actionName(),
+							Q.get(s).get(ja)
+						));
+						
+					}
+				}
+				
+					
+				// closes state tag
+				fileWriter.write("</state>\n\n");
+			}
+			
+			// closes xml root
+			fileWriter.write("</knowledge>\n"); 
+			fileWriter.close();
+		
+		} catch (IOException e) {
+			System.err.println("ERROR: Unable to save knowledge to file " + path);
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void loadKnowledge(String path) {
-		// TODO Auto-generated method stub
+		if (! (domain instanceof EnumerableSGDomain)){
+			throw new RuntimeException("Cannot loadKnowledge if domain is not a EnumerableSGDomain");
+		}
 		
+		//opens xml file
+		DocumentBuilder dBuilder = null;
+		Document doc = null;
+		try {
+			dBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			doc = dBuilder.parse(new File(path));
+		} catch (ParserConfigurationException|SAXException|IOException e) {
+			System.err.println("ERROR when parsing " + path +". knowledge NOT LOADED!");
+			e.printStackTrace();
+			return;
+		}
+		
+		//retrieves the game states, they'll be useful for filling the knowledge in
+		Map<String, State> nameToState = ((EnumerableSGDomain)domain).namesToStates();
+		
+		//traverses all 1st level nodes of xml file (children of root node) 
+		NodeList nList = doc.getDocumentElement().getChildNodes();
+		
+		for (int i = 0; i < nList.getLength(); i++){
+			Node n = nList.item(i);
+
+			//if node is 'learner', checks if ID is the same as mine
+			if (n.getNodeName() == "learner"){
+				Element e = (Element) n;
+				if(Integer.parseInt(e.getAttribute("id")) != agentNumber){
+					System.err.println(
+						"WARNING! Loading knowledge with agent with different ID. "
+						+ "I'll be probably loading a transposed reward matrix >:("
+					);
+				}
+			}
+			
+			//if node is 'state', loads its value and that of joint actions for it
+			else if (n.getNodeName() == "state"){
+				Element e = (Element) n;
+				
+				//FIXME works only for AggregateDiffState
+				String stateID = e.getAttribute("id");
+				//AggregateDiffState state = AggregateDiffState.fromString(stateID);
+				MicroRTSState state = (MicroRTSState) nameToState.get(stateID);
+				V.put(state, Double.parseDouble(e.getAttribute("value")));
+				
+				// checks if need to allocate Q
+				if(! Q.containsKey(state)){
+					Q.put(state, new HashMap<>());
+				}
+				
+				//jaNode stands for joint action node
+				for(Node jaNode = n.getFirstChild(); jaNode != null; jaNode = jaNode.getNextSibling()){
+					
+					if (jaNode.getNodeType() != Node.ELEMENT_NODE) continue;	//prevents ClassCastException
+					
+					// process the node, filling in the joint action value
+					Element jaElement = (Element) jaNode;
+					
+					// names of action components are separated by semicolon
+					String names[] = jaElement.getAttribute("name").split(";");
+					
+					// fills the list of joint action components
+					List<Action> components = new ArrayList<>();
+					for (String name : names){
+						components.add(
+							this.domain.getActionType(name).associatedAction(null)
+						);
+					}
+					
+					// retrieves the joint action and sets its value
+					JointAction ja = new JointAction(components);
+					//JAQValue jaq = myQSource.getQValueFor(s, ja); 
+					Q.get(state).put(ja, Double.parseDouble(jaElement.getAttribute("value")));
+				}
+			}
+		}
 	}
 	
 	/**
@@ -291,6 +447,10 @@ public class BackwardInduction implements PersistentLearner {
 		System.out.println("Solving...");
 		bi.solve((MicroRTSState) domain.getInitialState());
 		System.out.println("\nSolved.");
+		
+		System.out.println("Saving...");
+		bi.saveKnowledge("/tmp/backward-induction.xml");
+		System.out.println("Done.");
 		
 	}
 }
