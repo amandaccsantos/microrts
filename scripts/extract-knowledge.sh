fc2d17900f678df2804db57ede675701d2d491c4
anderson <andersonrochatavares@gmail.com>
2017-05-24 07:40:35 -0300

    new script to extract q_learner_final.txt and rename it properly

---
+++ scripts/extract-knowledge.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+cwd=$(pwd)
+
+mkdir /tmp/extracted/
+cd /tmp/extracted/
+
+# extraction
+for i in "$1"/*.tar.gz; do
+	echo "Extracting from $i"
+	tar xf $i --no-anchored q_learner_final.txt
+done
+
+# renaming
+for i in ./*; do
+	mkdir "$1/$i"
+	echo "Renaming inside $i"
+	for j in $(seq -f "%02g" 1 $2); do
+		mv "$i/rep$j/q_learner_final.txt" "$1/$i/q_final_rep$j.xml"
+	done
+done
+
+cd $cwd
+	
+echo "Done. Resulting files are in directories inside $i"
