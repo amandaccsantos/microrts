dcdf82081e3af66fd43a17607ab87f4a83958892
anderson <andersonrochatavares@gmail.com>
2017-08-01 16:42:59 -0300

    added more parameters and the script now packs the policies

--- scripts/extract-knowledge.sh
+++ scripts/extract-knowledge.sh
@@ -2,35 +2,45 @@
 
 
 if [ "$#" -lt 3 ]; then
-	echo "Usage: ./extract-knowledge.sh file destdir num-reps"
+echo "Usage: ./extract-knowledge.sh gzfile num-reps original_name destdir dest_prefix"
 	exit
 fi
 
 cwd=$(pwd)
+
+gzfile="$1"
+num_reps=$2
+original_name=$3
+destdir=$4
+dest_name=$5
 
 # creates temp. dir to extract files
 mkdir /tmp/extracted
 cd /tmp/extracted
 
 # extraction
-echo "Extracting from $1"
-tar xvf $1 --no-anchored q_learner_final.txt
+echo "Extracting from $gzfile"
+tar xvf "$gzfile" --no-anchored $original_name
 
 # goes to destination directory
 cd $cwd
-mkdir $2
-cd $2
+mkdir $destdir
+cd $destdir
 
-echo "Moving files to $2"
-for j in $(seq -f "%02g" 1 $3); do
+echo "Moving files to $destdir"
+for j in $(seq -f "%02g" 1 $num_reps); do
 	# using * because I assume there's only one directory inside extracted
-	mv /tmp/extracted/*/rep"$j"/q_learner_final.txt "q_final_rep$j.xml"
+	mv /tmp/extracted/*/rep"$j"/"$original_name" "$dest_name$j.xml"
 done
 
 # cleanup
 echo "Cleaning up"
 rm -rf /tmp/extracted
 
+# gzip
+echo "Packing files..."
+tar cfz "$destdir.tar.gz" *
+
 cd $cwd
 
-echo "Done. Resulting files are in $2"
+echo "Done. Resulting files are in $destdir. They are also packed in a directory above $destdir"
